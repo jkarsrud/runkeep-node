214742d0c0dded1e391812fbc545358af6b9390d
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2012-02-04 18:07:49 +0100

    Added OAuth library instead of building request myself atm

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 var api = require(__dirname + '/lib/runkeeperapi').RunKeeperApi,
+    connect = require('connect'),
+    http = require('http'),
     HealthGraph = require(__dirname + '/lib/HealthGraph');
 
 var opts = {
     clientId: '9e28e72bee7d44359f89af8e308a2abe',
-    clientSecret: '2fc22946a58d441792fa92d80f48c887'
-
+    clientSecret: '2fc22946a58d441792fa92d80f48c887',
+    redirectUrl: 'http://strengthapp.jit.su'
 };
+
+connect.createServer(function(req, res) {
+
+});
 
 var client = new HealthGraph(opts);
 
-//client.connectUser();
-client.getAccessToken('b869b75486f848fe84c7b80b079d40a9', function(token) {
-    console.log(token);
+
+
+client.getOAuthAccessToken('1f6230e2beac4800b2f9653ecc99738c', function(error, auth_token, refresh_token) {
+    console.log(error);
+    console.log(auth_token);
+    console.log(refresh_token);
 });
