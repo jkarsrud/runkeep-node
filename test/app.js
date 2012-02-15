0a069e7aed3aed6eea912354fd723c2d3d63f9f4
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2012-02-15 21:12:56 +0100

    added some proper tests for access and authorization

--- test/app.js
+++ test/app.js
@@ -13,6 +13,8 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'little india'}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -30,7 +32,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-app.post('/authenticate', routes.authenticate);
+app.get('/authcallback', routes.authcallback);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- test/routes/index.js
+++ test/routes/index.js
@@ -1,12 +1,48 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+var underscore = require('underscore'),
+    HealthGraph = require('../../lib/HealthGraph');
+
+var opts = {
+    clientId:'9e28e72bee7d44359f89af8e308a2abe',
+    clientSecret:'2fc22946a58d441792fa92d80f48c887',
+    authorizationUrl:'https://runkeeper.com/apps/authorize',
+    accessTokenUrl:'https://runkeeper.com/apps/token',
+    redirectUrl:'http://localhost:3000/authcallback'
 };
 
-exports.authenticate = function(req, res) {
-    res.render('authenticate', {title: 'Authenticate'});
+exports.index = function (req, res) {
+    var params = {
+        client_id:opts.clientId,
+        response_type:'code',
+        redirect_uri:opts.redirectUrl
+    };
+
+    var body = underscore.map(params,
+        function (val, key) {
+            return key + "=" + val;
+        }).join('&');
+
+    var authLink = opts.authorizationUrl + '?' + body;
+
+    res.render('index', { title:'Express', authLink:authLink});
+};
+
+exports.authcallback = function (req, res) {
+    var code = req.query.code;
+    var hg = new HealthGraph(opts);
+
+    hg.getOAuthAccessToken(code, function (error, accessToken, refreshToken) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            req.session.accessToken = accessToken;
+            req.session.refreshToken = refreshToken;
+
+            res.render('callback', {title:'Authenticated', accessToken:req.session.accessToken, refreshToken: req.session.refreshToken});
+        }
+    });
 };
